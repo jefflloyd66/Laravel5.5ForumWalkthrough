40fb0b0b8725a2ccc3e19a3328d06e72dbb9c068
Jeff <jeff@home>
2017-10-20 18:55:40 +0100

    4: Authenticated users may respond to threads

--- forum/tests/Feature/ThreadTest.php
+++ forum/tests/Feature/ThreadTest.php
@@ -4,8 +4,8 @@ namespace Tests\Feature;
 
 use App\Thread;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ThreadTest extends TestCase
 {
@@ -32,16 +32,41 @@ class ThreadTest extends TestCase
     /** @test */
     public function a_user_can_browse_a_single_thread()
     {
-        $response = $this->get('/threads/' . $this->thread->id);
+        $response = $this->get($this->thread->path());
         $response->assertSee($this->thread->title);
+        $response->assertSee($this->thread->creator->name);
     }
 
     /** @test */
     public function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
         $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
-        $response = $this->get('/threads/' . $this->thread->id);
+        $response = $this->get($this->thread->path());
         $response->assertSee($reply->owner->name);
         $response->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function an_authenticated_user_may_participate_forum_threads()
+    {
+        $this->signIn($user = factory('App\User')->create());
+        $thread = factory('App\Thread')->create();
+        $reply = factory('App\Reply')->create();
+
+        $this->post($thread->path() . '/replies', $reply->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory('App\Thread')->create();
+        $reply = factory('App\Reply')->create();
+
+        $this->post($thread->path() . '/replies', $reply->toArray());
     }
 }
