b310548e787c3292ce4200b714a60469e8ae2c85
Jeff <jeff@home>
2017-10-20 13:14:46 +0100

    3: a thread can have replies

--- forum/tests/Feature/ThreadTest.php
+++ forum/tests/Feature/ThreadTest.php
@@ -2,29 +2,46 @@
 
 namespace Tests\Feature;
 
+use App\Thread;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ThreadTest extends TestCase
 {
+    /** @var  Thread */
+    private $thread;
+
+
     use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
 
     /** @test */
     public function a_user_can_browse_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads');
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_browse_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($this->thread->title);
+    }
 
-        $response = $this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($reply->owner->name);
+        $response->assertSee($reply->body);
     }
 }
