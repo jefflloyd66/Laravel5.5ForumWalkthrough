2a38f0521406108d5ac447c17595e9b0e453f4c4
Jeff <jeff@home>
2017-10-21 15:06:43 +0100

    7: some test helper functions

--- forum/tests/Feature/CreateThreadsTest.php
+++ forum/tests/Feature/CreateThreadsTest.php
@@ -13,9 +13,9 @@ class CreateThreadsTest extends TestCase
     /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
-        $this->actingAs(factory('App\User')->create());
+        $this->signIn();
 
-        $thread = factory('App\Thread')->make();
+        $thread = make('App\Thread');
         $this->post('/threads', $thread->toArray());
 
         $response = $this->get($thread->path());
@@ -28,7 +28,7 @@ class CreateThreadsTest extends TestCase
     {
         $this->expectException('Illuminate\Auth\AuthenticationException');
 
-        $thread = factory('App\Thread')->make();
+        $thread = make('App\Thread');
         $this->post('/threads', $thread->toArray());
     }
 }
